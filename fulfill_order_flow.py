b2a34b632109555e3a4ab96087bec320ddedb491
Taylor Curran <taylorcurranagtc@gmail.com>
2024-09-10 15:55:24 -0700

    removing notes

--- fulfill_order_flow.py
+++ fulfill_order_flow.py
@@ -7,48 +7,6 @@ def fulfill_order_flow(deal_id: str = "1234", customer_id: str = "abcd"):
     print(f"Fulfilling order {deal_id}!")
 
 
-# Example Trigger
-
-#   triggers=[
-#             DeploymentCompoundTrigger(
-#                 enabled=True,
-#                 name="my-compound-trigger",
-#                 require="all",
-#                 triggers=[
-#                     {
-#                       "type": "event",
-#                       "match": {"prefect.resource.id": "my.external.resource"},
-#                       "expect": ["external.resource.pinged"],
-#                     },
-#                     {
-#                       "type": "event",
-#                       "match": {"prefect.resource.id": "my.external.resource"},
-#                       "expect": ["external.resource.replied"],
-#                     },
-#                 ],
-#                 parameters={
-#                     "param_1": "{{ event }}",
-#                 },
-#             )
-#         ],
-
-
-# emit_event(
-#     event=f"cart.completed.{deal_id}",
-#     resource={"prefect.resource.id": f"customer.{customer_id}"},
-# )
-# time.sleep(5)
-# emit_event(
-#     event=f"shipping_address.completed.{deal_id}",
-#     resource={"prefect.resource.id": f"customer.{customer_id}"},
-# )
-# time.sleep(5)
-# emit_event(
-#     event=f"payment.completed.{deal_id}",
-#     resource={"prefect.resource.id": f"customer.{customer_id}"},
-# )
-
-
 if __name__ == "__main__":
     fulfill_order_flow.serve(
         name="fulfill_order_flow",
